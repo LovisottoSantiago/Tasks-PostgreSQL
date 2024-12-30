13f2902f09b9652daae79cafbff91227c3904180
SantiagoLovisotto <santiagolovisotto@gmail.com>
2024-12-29 23:05:12 -0300

    test

--- Tasks/src/main/java/org/tasks/DatabaseConnection.java
+++ Tasks/src/main/java/org/tasks/DatabaseConnection.java
@@ -1,6 +1,9 @@
 package org.tasks;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,21 +13,39 @@ public class DatabaseConnection {
 
     private static final String URL = "jdbc:postgresql://localhost:5432/tasks_db";
 
-    public static Connection getConnection() {
+    public Connection getConnection() {
         try {
-            Scanner scanner = new Scanner(System.in);
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.print("Please, enter your username: ");
+                String username = scanner.nextLine();
+                System.out.print("Please, enter your password: ");
+                String password = scanner.nextLine();
 
-            System.out.print("Please, enter your username: ");
-            String username = scanner.nextLine();
-            System.out.print("Please, enter your password: ");
-            String password = scanner.nextLine();
-
-            return DriverManager.getConnection(URL, username, password);
+                return DriverManager.getConnection(URL, username, password);
+            }
         }
-        catch (Exception e) {
+        catch (SQLException e) {
             logger.log(Level.SEVERE, "An exception has occurred", e);
             return null;
         }
+    }
+
+    public void insertRow(Connection conn, String title, String description) {        
+        String query = "INSERT INTO tasks (title, description, created_at) VALUES (?, ?, ?)";
+        try (PreparedStatement statement = conn.prepareStatement(query)) {
+            statement.setString(1, title);
+            statement.setString(2, description);
+            statement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+
+            int rowsInserted = statement.executeUpdate();
+            if (rowsInserted > 0) {
+                    System.out.println("Row inserted");
+            }            
+        }
+        catch (Exception e) {
+            logger.log(Level.SEVERE, "Error inserting row into the database", e);
+        }
+
     }
 
 
--- Tasks/src/main/java/org/tasks/Main.java
+++ Tasks/src/main/java/org/tasks/Main.java
@@ -1,16 +1,30 @@
 package org.tasks;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Connection connection = DatabaseConnection.getConnection();
+        DatabaseConnection db = new DatabaseConnection();
+        Connection connection = db.getConnection();
 
         if (connection != null){
             System.out.println("Connection successfully");
         } else {
             System.out.println("Connection error");
         }
+
+        if (connection != null) {
+                    try (Scanner scanner = new Scanner(System.in)) {
+                        System.out.print("Enter task title: ");
+                        String title = scanner.nextLine();
+
+                        System.out.print("Enter task description: ");
+                        String description = scanner.nextLine();
+
+                        db.insertRow(connection, title, description);
+                    }
+                }
 
     }
 

4ea1e870b15285612a9e1465e6e5f4ab7126f577
SantiagoLovisotto <santiagolovisotto@gmail.com>
2025-01-05 23:50:50 -0300

    java swing

--- Tasks/src/main/java/org/tasks/DatabaseConnection.java
+++ Tasks/src/main/java/org/tasks/DatabaseConnection.java
@@ -1,4 +1,5 @@
 package org.tasks;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,100 +11,94 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class DatabaseConnection {
-    private static final Logger logger = Logger.getLogger(DatabaseConnection.class.getName());
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/tasks_db";
+		private static final Logger logger = Logger.getLogger(DatabaseConnection.class.getName());
 
-    // Connection with the database
-    public Connection getConnection(String username, String password) {
-        try {
-            return DriverManager.getConnection(URL, username, password);
-        } catch (SQLException e) {
-            logger.log(Level.SEVERE, "An exception has occurred", e);
-            return null;
-        }
-    }
+		private static final String URL = "jdbc:postgresql://localhost:5432/tasks_db";
 
+		// Connection with the database
+		public Connection getConnection(String username, String password) {
+				try {
+						return DriverManager.getConnection(URL, username, password);
+				} catch (SQLException e) {
+						logger.log(Level.SEVERE, "An exception has occurred", e);
+						return null;
+				}
+		}
 
 //! <---------- SHOW ALL ITEMS METHOD ---------->
-    public void showItems(Connection conn) {
-        String query = "SELECT * FROM public.tasks ORDER BY id ASC";
-        try (PreparedStatement statement = conn.prepareStatement(query);
-            ResultSet resultSet = statement.executeQuery()) {
-            
-            while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String title = resultSet.getString("title");                
-                String description = resultSet.getString("description");                
-                System.out.println("ID: " + id + ", Title: " + title + ", " + description);
-            }
-        } catch (Exception e) {
-            logger.log(Level.SEVERE, "Error trying to show items in the database");
-        }
-    }
+		public void showItems(Connection conn) {
+				String query = "SELECT * FROM public.tasks ORDER BY id ASC";
+				try (PreparedStatement statement = conn.prepareStatement(query); ResultSet resultSet = statement.executeQuery()) {
+
+						while (resultSet.next()) {
+								int id = resultSet.getInt("id");
+								String title = resultSet.getString("title");
+								String description = resultSet.getString("description");
+								System.out.println("ID: " + id + ", Title: " + title + ", " + description);
+						}
+				} catch (Exception e) {
+						logger.log(Level.SEVERE, "Error trying to show items in the database");
+				}
+		}
 
 //! <---------- INSERT METHOD ---------->
-    public void insertRow(Connection conn, String title, String description) {        
-        String query = "INSERT INTO tasks (title, description, created_at) VALUES (?, ?, ?)";
-        try (PreparedStatement statement = conn.prepareStatement(query)) {
-            statement.setString(1, title);
-            statement.setString(2, description);
-            statement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
+		public void insertRow(Connection conn, String title, String description) {
+				String query = "INSERT INTO tasks (title, description, created_at) VALUES (?, ?, ?)";
+				try (PreparedStatement statement = conn.prepareStatement(query)) {
+						statement.setString(1, title);
+						statement.setString(2, description);
+						statement.setTimestamp(3, new Timestamp(System.currentTimeMillis()));
 
-            int rowsInserted = statement.executeUpdate();
-            if (rowsInserted > 0) {
-                    System.out.println("Row inserted");
-            }            
-        }
-        catch (Exception e) {
-            logger.log(Level.SEVERE, "Error inserting row into the database", e);
-        }
-    }
+						int rowsInserted = statement.executeUpdate();
+						if (rowsInserted > 0) {
+								System.out.println("Row inserted");
+						}
+				} catch (Exception e) {
+						logger.log(Level.SEVERE, "Error inserting row into the database", e);
+				}
+		}
 
 //! <---------- DELETE METHOD ---------->
-    public void deleteRow(Connection conn, int ID) {
-        String query = "SELECT tasks.id AS ID, tasks.title AS TITLE FROM public.tasks WHERE tasks.id = ?";
-        String deleteQuery = "DELETE FROM public.tasks WHERE tasks.id = ?";
+		public void deleteRow(Connection conn, int ID) {
+				String query = "SELECT tasks.id AS ID, tasks.title AS TITLE FROM public.tasks WHERE tasks.id = ?";
+				String deleteQuery = "DELETE FROM public.tasks WHERE tasks.id = ?";
 
-        try (PreparedStatement statement = conn.prepareStatement(query)) {
-            statement.setInt(1, ID);
+				try (PreparedStatement statement = conn.prepareStatement(query)) {
+						statement.setInt(1, ID);
 
-            try (ResultSet resultSet = statement.executeQuery()) {
-                @SuppressWarnings("resource")
-                Scanner scanner = new Scanner(System.in);
+						try (ResultSet resultSet = statement.executeQuery()) {
+								@SuppressWarnings("resource")
+								Scanner scanner = new Scanner(System.in);
 
-                while (resultSet.next()) {
-                    int id = resultSet.getInt("ID");
-                    String title = resultSet.getString("TITLE");
+								while (resultSet.next()) {
+										int id = resultSet.getInt("ID");
+										String title = resultSet.getString("TITLE");
 
-                    System.out.println("You are going to delete: ");
-                    System.out.println("ID: " + id + ", Title: " + title);
-                    System.out.println("Is it okay? Y/N: ");
-                    String answer = scanner.nextLine();
-                    
-                    if (answer.toUpperCase().equals("Y")) {
-                        try (PreparedStatement deleteStatement = conn.prepareStatement(deleteQuery)) {
-                            deleteStatement.setInt(1, ID);
+										System.out.println("You are going to delete: ");
+										System.out.println("ID: " + id + ", Title: " + title);
+										System.out.println("Is it okay? Y/N: ");
+										String answer = scanner.nextLine();
 
-                            int rowsAffected = deleteStatement.executeUpdate();
-                            if (rowsAffected > 0) {
-                                System.out.println("The task was successfully deleted.\n");
-                            } else {
-                                System.out.println("No task found with the given ID.\n");
-                            }
-                        }
-                    }
-                    else {
-                        System.out.println("Nothing happened.\n");
-                    }
-                }
-            }
-        }   
-        catch (Exception e) {
-            logger.log(Level.SEVERE, "Error finding this item in the database", e);
-        }
-    }
+										if (answer.toUpperCase().equals("Y")) {
+												try (PreparedStatement deleteStatement = conn.prepareStatement(deleteQuery)) {
+														deleteStatement.setInt(1, ID);
 
-
+														int rowsAffected = deleteStatement.executeUpdate();
+														if (rowsAffected > 0) {
+																System.out.println("The task was successfully deleted.\n");
+														} else {
+																System.out.println("No task found with the given ID.\n");
+														}
+												}
+										} else {
+												System.out.println("Nothing happened.\n");
+										}
+								}
+						}
+				} catch (Exception e) {
+						logger.log(Level.SEVERE, "Error finding this item in the database", e);
+				}
+		}
 
 }
--- Tasks/src/main/java/org/tasks/Main.java
+++ Tasks/src/main/java/org/tasks/Main.java
@@ -4,79 +4,81 @@ import java.sql.Connection;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
-        try (Scanner scanner = new Scanner(System.in)) {
-            DatabaseConnection db = new DatabaseConnection();
+		public static void main(String[] args) {
 
-            System.out.print("Please, enter your username: ");
-            String username = scanner.nextLine();
+				try (Scanner scanner = new Scanner(System.in)) {
+						DatabaseConnection db = new DatabaseConnection();
 
-            System.out.print("Please, enter your password: ");
-            String password = scanner.nextLine();
+						System.out.print("Please, enter your username: ");
+						String username = scanner.nextLine();
 
-            Connection connection = db.getConnection(username, password);
+						System.out.print("Please, enter your password: ");
+						String password = scanner.nextLine();
 
-            if (connection != null) {
-                System.out.println("Connection successfully\n");
-                boolean menu = true; 
-                while (menu) {
-                    System.out.println("███╗   ███╗███████╗███╗   ██╗██╗   ██╗");
-                    System.out.println("████╗ ████║██╔════╝████╗  ██║██║   ██║");
-                    System.out.println("██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║");
-                    System.out.println("██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║");
-                    System.out.println("██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝");
-                    System.out.println("╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ ");                                     
-                    System.out.println("1. Insert a new row");
-                    System.out.println("2. Delete a row");
-                    System.out.println("3. Show items");
-                    System.out.println("4. Exit");
-                    System.out.print("Enter your option: ");
-                    try {
-                        String option = scanner.nextLine();
+						Connection connection = db.getConnection(username, password);
 
-                        switch (option) {
-                            case "1" -> {
-                                System.out.print("Enter task title: ");
-                                String title = scanner.nextLine();
+						if (connection != null) {
+								System.out.println("Connection successfully\n");
+								boolean menu = true;
+								while (menu) {
+										System.out.println("███╗   ███╗███████╗███╗   ██╗██╗   ██╗");
+										System.out.println("████╗ ████║██╔════╝████╗  ██║██║   ██║");
+										System.out.println("██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║");
+										System.out.println("██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║");
+										System.out.println("██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝");
+										System.out.println("╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ ");
+										System.out.println("1. Insert a new row");
+										System.out.println("2. Delete a row");
+										System.out.println("3. Show items");
+										System.out.println("4. Exit");
+										System.out.print("Enter your option: ");
+										try {
+												String option = scanner.nextLine();
 
-                                System.out.print("Enter task description: ");
-                                String description = scanner.nextLine();
+												switch (option) {
+														case "1" -> {
+																System.out.print("Enter task title: ");
+																String title = scanner.nextLine();
 
-                                db.insertRow(connection, title, description);
-                                System.out.println("\nTouch any key to return to the menu");
-                                scanner.nextLine();
-                            }
-                            case "2" -> {
-                                db.showItems(connection);
-                                System.out.print("Enter the ID of the task to delete: ");
-                                int taskId = scanner.nextInt();
-                                scanner.nextLine();
-                                db.deleteRow(connection, taskId);
-                                System.out.println("\nTouch any key to return to the menu");
-                                scanner.nextLine();
-                            }
-                            case "3" -> {
-                                db.showItems(connection);                             
-                                System.out.println("\nTouch any key to return to the menu");
-                                scanner.nextLine();
-                            }
-                            case "4" -> {
-                                System.out.println("Exiting the application. Goodbye!");
-                                menu = false;      
-                                System.out.println(menu);                          
-                                System.exit(0);                                
-                            }
-                            default -> System.out.println("Invalid option. Please try again.");
-                        }
-                    } catch (Exception e) {
-                        System.out.println("An error occurred: " + e.getMessage());
-                        scanner.nextLine();
-                    }
-                }
-            } else {
-                System.out.println("Connection error");
-            }
-        }
-    }
+																System.out.print("Enter task description: ");
+																String description = scanner.nextLine();
+
+																db.insertRow(connection, title, description);
+																System.out.println("\nTouch any key to return to the menu");
+																scanner.nextLine();
+														}
+														case "2" -> {
+																db.showItems(connection);
+																System.out.print("Enter the ID of the task to delete: ");
+																int taskId = scanner.nextInt();
+																scanner.nextLine();
+																db.deleteRow(connection, taskId);
+																System.out.println("\nTouch any key to return to the menu");
+																scanner.nextLine();
+														}
+														case "3" -> {
+																db.showItems(connection);
+																System.out.println("\nTouch any key to return to the menu");
+																scanner.nextLine();
+														}
+														case "4" -> {
+																System.out.println("Exiting the application. Goodbye!");
+																menu = false;
+																System.out.println(menu);
+																System.exit(0);
+														}
+														default ->
+																System.out.println("Invalid option. Please try again.");
+												}
+										} catch (Exception e) {
+												System.out.println("An error occurred: " + e.getMessage());
+												scanner.nextLine();
+										}
+								}
+						} else {
+								System.out.println("Connection error");
+						}
+				}
+		}
 }
